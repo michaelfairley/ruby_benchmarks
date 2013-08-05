8ee6935786a9b1a23d450bd7a1767bd0ba1829b8
Michael Fairley <michaelfairley@gmail.com>
2013-08-04 22:30:59 -0500

    Another VBO packing idea

--- vbo_packing.rb
+++ vbo_packing.rb
@@ -13,7 +13,21 @@ end
 things = 100000.times.map{|i| Thing.new(i) }
 
 Benchmark.bmbm(10) do |x|
-  x.report("flat_map") { 10.times { things.flat_map(&:values).pack('f*') } }
-  x.report("join") { 10.times { things.map{|t| t.values.pack('f*')}.join } }
-  x.report("concat") { 10.times { things.each_with_object("") {|t,s| s << t.values.pack('f*') } } }
+  x.report("flat_map") do
+    10.times { things.flat_map(&:values).pack('f*') }
+  end
+  x.report("join") do
+    10.times { things.map{|t| t.values.pack('f*')}.join }
+  end
+  x.report("concat") do
+    10.times { things.each_with_object("") {|t,s| s << t.values.pack('f*') } }
+  end
+  x.report("prealloc") do
+    10.times do
+      result = " " * things.size * 6 * 4
+      things.each_with_index do |thing, i|
+        result[i*24, 24] = thing.values.pack('f*')
+      end
+    end
+  end
 end
